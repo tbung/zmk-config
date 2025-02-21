2a7c6ae8bafb48adc3da23a3414842955970e040
Till Bungert <tillbungert@gmail.com>
2025-02-21 20:17:25 +0100

    fix: buffer overflow in layer display name

--- boards/shields/nice_view_poke/widgets/layer.c
+++ boards/shields/nice_view_poke/widgets/layer.c
@@ -6,12 +6,12 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
 
-    char text[10] = {};
+    char text[11] = {};
 
     if (state->layer_label == NULL) {
         sprintf(text, "Layer %i", state->layer_index);
     } else {
-        strcpy(text, state->layer_label);
+        strncpy(text, state->layer_label, 10);
         to_uppercase(text);
     }
 
